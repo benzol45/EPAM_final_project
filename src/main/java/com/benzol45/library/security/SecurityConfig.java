3b350ef45b5636f43201b6e3788e42e9fa513455
Михаил Калинин <benzol.45@gmail.com>
2023-04-12 15:35:11 +0200

    add default admin with credentials "admin/admin"

--- src/main/java/com/benzol45/library/security/SecurityConfig.java
+++ src/main/java/com/benzol45/library/security/SecurityConfig.java
@@ -15,8 +15,8 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-        //return new BCryptPasswordEncoder();
+        //return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
---
+++ src/main/resources/db/changelog/v-2.0.0/add_default_admin.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (full_name, contact, language, login, password, is_blocked, role)
+        VALUES ('Default admin', '', 'EN', 'admin', '$2a$10$EB27YTkV0UXw9UamWolwPekNktBwG3HsCDJQtw5oysFD6Z62LnYRi', false, 'ADMINISTRATOR');
