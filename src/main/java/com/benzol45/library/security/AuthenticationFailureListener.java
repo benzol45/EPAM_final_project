db9a751049edd7f6cf4d3da69b41601e369296c2
Михаил Калинин <benzol.45@gmail.com>
2023-04-30 22:53:12 +0200

    add incorrect login loging

---
+++ src/main/java/com/benzol45/library/security/AuthenticationFailureListener.java
@@ -0,0 +1,23 @@
+package com.benzol45.library.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class AuthenticationFailureListener implements ApplicationListener<AuthenticationFailureBadCredentialsEvent> {
+
+    public void onApplicationEvent(AuthenticationFailureBadCredentialsEvent event) {
+        String ip = "";
+        try {
+            ip = ((WebAuthenticationDetails)((UsernamePasswordAuthenticationToken)event.getSource()).getDetails()).getRemoteAddress();
+        } catch (ClassCastException e) {
+        }
+
+        log.warn("Authentication failed for user " + event.getAuthentication().getPrincipal() + " from address " + ip);
+    }
+}
